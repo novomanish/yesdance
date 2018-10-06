c90251fec7237b3c2afaca585d823ab6f8ef2cd2
Manish Jaiswal <manish.jaiswal@logmein.com>
2018-10-06 11:43:26 -0700

    specs

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/customers');
+const customerRouter = require('./routes/customer');
 
 const app = express();
 
@@ -15,6 +15,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/api/customers', usersRouter);
+app.use('/api/customer', customerRouter);
 
 module.exports = app;
