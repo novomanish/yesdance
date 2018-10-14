c8c4a6be1af71992f1e66a082395ca6e21989bdb
Manish Jaiswal <manish.jaiswal@logmein.com>
2018-10-14 12:12:31 -0700

    event api with join

--- api/src/helper/dao.js
+++ api/src/helper/dao.js
@@ -6,15 +6,29 @@ const config = require('../config/db');
 
 exports.get = function get(table) {
   return new Promise(((resolve) => {
-    const sql = new SQLBuilder('MySQL');
-    const customersSQL = sql.$select({
+    const sqlBuilder = new SQLBuilder('MySQL');
+    const sqlQuery = sqlBuilder.$select({
       $from: `${config.database}.${table}`,
     });
-    console.log(customersSQL);
+    console.log(sqlQuery);
 
 
     mysql.query(
-      customersSQL,
+      sqlQuery,
+      (err, results) => {
+        if (err) {
+          throw err;
+        }
+        resolve(results);
+      },
+    );
+  }));
+};
+
+exports.query = function get(sqlQuery) {
+  return new Promise(((resolve) => {
+    mysql.query(
+      sqlQuery,
       (err, results) => {
         if (err) {
           throw err;
--- api/src/routes/event.js
+++ api/src/routes/event.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const dao = require('../helper/dao');
+const config = require('../config/db');
+
 
 router.get('/', (req, res) => {
-  dao.get('event').then((results) => {
+  dao.query(`select e.id, e.type, e.startdate, e.enddate, p.name, p.id eventid from ${config.database}.event as e left join ${config.database}.product p on p.id = e.type;`).then((results) => {
     res.send(results);
   });
 });
