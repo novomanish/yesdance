edc0a6e1c05347f9472f9a16e314f449b87232bc
Manish Jaiswal <manish.jaiswal@logmein.com>
2018-10-28 09:57:28 -0700

    async await

--- api/src/routes/quote.js
+++ api/src/routes/quote.js
@@ -2,10 +2,9 @@ const router = require('express').Router();
 const dao = require('../helper/dao');
 const product = require('./product');
 
-router.post('/', (req, res) => {
-  product.getProductDetails(req.body.products).then((products) => {
-    res.send(products);
-  });
+router.post('/', async (req, res) => {
+  const products = await product.getProductDetails(req.body.products);
+  res.send(products);
 });
 
 module.exports = router;
